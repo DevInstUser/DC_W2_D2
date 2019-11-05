7114106a06cd7706111ab2934c8a528eabd7f0c1
root <dzafrilla@yahoo.fr>
2019-11-05 17:28:08 +0200

    Week2_Day2

---
+++ DC_W2_D2.js
@@ -0,0 +1,27 @@
+
+/// array 1 ////
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// 1. Remove the Banana from the array.
+array.splice(0,1);
+
+// 2. Sort the array in order.
+array.sort();
+
+// 3. Put “Kiwi” at the end of the array.
+array.push("Kiwi");
+
+// 4. Remove “Apples” from the array.
+array.splice(0,1);
+// You should have at the end:[“Kiwi”, “Oranges”, “Blueberries”]
+console.log(array);
+
+/// array 2 ///
+
+var array2 = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// access “Oranges”.
+array2[2];
+
+
+
